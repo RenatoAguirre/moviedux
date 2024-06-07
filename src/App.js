e9c68ea4bb2bdc10d98757a1ce7c2ee67ed0ad53
RenatoAguirre <raaguirre1@miuandes.cl>
2024-06-07 00:48:54 -0400

    feat: added routes

--- src/App.js
+++ src/App.js
@@ -3,14 +3,31 @@ import "./styles.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import MoviesGrid from "./components/MoviesGrid";
+import WatchList from "./components/WatchList";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <Header></Header>
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Movies</Link>
+              </li>{" "}
+              <li>
+                <Link to="/watchlist">WatchList</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/" element={<MoviesGrid />} />
+            <Route path="/watchlist" element={<WatchList />} />
+          </Routes>
+        </Router>
       </div>
-      <MoviesGrid></MoviesGrid>
       <Footer></Footer>
     </div>
   );
